2f714ae15605a62badcadbbc4139e481373966fa
Kerby Shedden <kshedden@umich.edu>
2018-10-07 21:58:06 -0400

    update

--- Medicare_opioids/opioids.py
+++ Medicare_opioids/opioids.py
@@ -1,3 +1,15 @@
+# This script requires statsmodels version 0.9 or above.  If your statsmodels
+# is too old, use this shell command to update it:
+#     pip install --upgrade --user statsmodels
+
+# If True, do not run the procedure analysis, which requires a lot of memory
+# and may hang (in TruncatedSVD)
+noproc = True
+
+# The following two lines are needed on flux and some other headless systems
+import matplotlib
+matplotlib.use('agg')
+
 import patsy
 import pandas as pd
 import numpy as np
@@ -66,69 +78,97 @@ olsresultz = olsmodelz.fit()
 # Fit log opioid versus non-opioid volume separately by state
 plt.clf()
 bxr = np.linspace(-4, 4, 20)
-state_params = []
-for dy in dr.groupby("state"):
-    sn = dy[0] # State abbreviation
-    dz = dy[1] # Data
+for sn, dz in dr.groupby("state"):
+    # sn is the state's name, dz is the data for that state
     model = sm.OLS.from_formula("log_op_z ~ log_nonop_z", data=dz)
     result = model.fit()
     params = result.params.values
-    state_params.append(params)
     plt.plot(bxr, params[0] + params[1]*bxr, '-', color='grey')
 plt.title("Trends by state")
 plt.xlabel("Log non-opioid days supply (Z-score)")
 plt.ylabel("Log opioid days supply (Z-score)")
 pdf.savefig()
 
-state_params = np.asarray(state_params)
-
 pdf.close()
 
-# Fit models for opioid relative to non-opioid prescribing, looking also at
-# state-specific intercepts and slopes
+# Fit three different fixed effects models for opioid volume relative to non-opioid volume.
+
+# Allow the intercepts to vary by state (no z-scoring)
 femodel = sm.OLS.from_formula("log_op ~ log_nonop + C(state)", data=dr)
 feresult = femodel.fit()
+
+# Allow the intercepts to vary by state (with z-scoring)
 femodelz = sm.OLS.from_formula("log_op_z ~ log_nonop_z + C(state)", data=dr)
 feresultz = femodelz.fit()
+
+# Allow the slopes and intercepts to vary by state (with z-scoring)
 femodelsz = sm.OLS.from_formula("log_op_z ~ log_nonop_z * C(state)", data=dr)
 feresultsz = femodelsz.fit()
 
-# Pairwise comparison of state intercepts, for every pair of states
-# Important to use centered (or Z-scored) data here so the intercepts
-# are inside the range of the data.
-pa = feresultz.params
-vc = feresultz.cov_params()
+# Pairwise comparison of state intercepts, for every pair of states.
+# It's important here to use centered (or Z-scored) data here so the
+# intercepts are inside the range of the data.
+
+pa = feresultz.params # Parameter estimates
+vc = feresultz.cov_params() # Covariance matrix of parameter estimates
+
+# Positions of the state fixed effects in the parameter vector
 ii = [i for i,x in enumerate(pa.index) if x.startswith("C(state")]
+
+# Reduce the parameter estimate vector and covariance matrix to contain
+# only the parameters corresponding to state effects
 pa = pa.iloc[ii]
 vc = vc.iloc[ii, ii]
+
+# Get the contrast for every pair of states
 zc = np.zeros((len(pa), len(pa)))
 for i1 in range(len(pa)):
     for i2 in range(len(pa)):
+
+        # Create a contrast vector between states i1 and i2
         d = np.zeros(len(pa))
         d[i1] = 1
         d[i2] = -1
+
+        # Get the contrast and standard error (for comparing these two states)
         se = np.sqrt(np.dot(d, np.dot(vc, d)))
         zc[i1, i2] = (pa[i1] - pa[i2]) / se
-j1, j2 = np.tril_indices(len(pa), -1)
+
+# Put the results in a dataframe with columns showing the two states
+# being compared, and the Z-score for the difference of the state interepts
+j1, j2 = np.tril_indices(len(pa), -1) # The lower triangle so we don't repeat pairs
 statepair_icept_z = pd.DataFrame({"State1": pa.index.values[j1],
                                   "State2": pa.index.values[j2],
                                   "Diff": zc[j1, j2]})
 statepair_icept_z = statepair_icept_z.sort_values(by="Diff")
 
 # Pairwise comparison of state slopes, for every pair of states
-pa = feresultsz.params
-vc = feresultsz.cov_params()
+# See preceeding section, the code is very simuilar except here we
+# work with the slope parameters instead of the intercept parameters
+
+pa = feresultsz.params # All parameter estimates
+vc = feresultsz.cov_params() # Covariance matrix for all parameter estiamtes
+
+# Restrict to only the slope parameter estimates
 ii = [i for i,x in enumerate(pa.index) if x.startswith("log_nonop_z:C(state")]
 pa = pa.iloc[ii]
 vc = vc.iloc[ii, ii]
+
 zcx = np.zeros((len(pa), len(pa)))
 for i1 in range(len(pa)):
     for i2 in range(len(pa)):
+
+        # Contrast vector
         d = np.zeros(len(pa))
         d[i1] = 1
         d[i2] = -1
+
+        # Standard error of the contrast
         se = np.sqrt(np.dot(d, np.dot(vc, d)))
+
+        # Z-score of the contrast
         zcx[i1, i2] = (pa[i1] - pa[i2]) / se
+
 j1, j2 = np.tril_indices(len(pa), -1)
 statepair_slope_z = pd.DataFrame({"State1": pa.index.values[j1],
                                   "State2": pa.index.values[j2],
@@ -137,13 +177,15 @@ statepair_slope_z = statepair_slope_z.sort_values(by="Diff")
 
 # Mean/variance analysis for basic model
 dr["fit"] = olsresult.fittedvalues
-dr["bin"] = pd.qcut(dr.fit, 20)
+dr["bin"] = pd.qcut(dr.fit, 20) # Slice the fitted values into 20 bins
 meanvar_base = dr.groupby("bin")["fit", "log_op"].agg({"fit": np.mean, "log_op": np.var})
+meanvar_base = meanvar_base.rename(columns={"fit": "mean", "log_op": "var"})
 
 # Mean/variance analysis for state-adjusted model
 dr["fit"] = feresult.fittedvalues
-dr["bin"] = pd.qcut(dr.fit, 20)
+dr["bin"] = pd.qcut(dr.fit, 20) # Slice the fitted values into 20 bins
 meanvar_states = dr.groupby("bin")["fit", "log_op"].agg({"fit": np.mean, "log_op": np.var})
+meanvar_states = meanvar_states.rename(columns={"fit": "mean", "log_op": "var"})
 
 # Basic mixed model with intercepts that vary by state ("random intercepts")
 memodel_i = sm.MixedLM.from_formula("log_op_z ~ log_nonop_z", groups="state", data=dr)
@@ -151,8 +193,8 @@ meresult_i = memodel_i.fit()
 
 # Mixed model with slopes that vary by state ("random slopes")
 memodel_is = sm.MixedLM.from_formula("log_op_z ~ log_nonop_z", groups="state",
-                                   vc_formula={"i": "0 + C(state)", "s": "0+log_nonop_z:C(state)"},
-                                   data=dr)
+                                     vc_formula={"i": "0 + C(state)", "s": "0+log_nonop_z:C(state)"},
+                                     data=dr)
 meresult_is = memodel_is.fit()
 
 # Basic mixed model for cities (random intercepts by city)
@@ -167,20 +209,39 @@ du = db.groupby("npi")["provider_type"].agg("first")
 du = pd.DataFrame(du).reset_index()
 dr = pd.merge(dr, du, left_on="npi", right_on="npi")
 
-# Use LARS to consider provider effects
+# Use LARS to consider provider type effects.  LARS can't accept a formula, so we
+# construct the response vector and design matrix outside of LARS then pass them
+# in.
 y, x = patsy.dmatrices("log_op_z ~ 0 + log_nonop_z + C(provider_type)", data=dr,
                        return_type='dataframe')
-xa = np.asarray(x)
-ya = np.asarray(y)[:, 0]
-xnames = x.columns.tolist()
-alphas, active, coefs = linear_model.lars_path(xa, ya, method='lars', verbose=True)
 
-# Display the first few variables selected by lars and the fitted
-# correlation.
-for k in range(1, 20):
-    print(xnames[active[k-1]])
+# Standardize
+xa = np.asarray(x)
+xa -= xa.mean(0)
+xa /= xa.std(0)
+ya = np.asarray(y)[:, 0]
+ya -= ya.mean(0)
+ya /= ya.std(0)
+xnames = x.columns.tolist()
+
+# Run LARS
+alphas, active, coefs = linear_model.lars_path(xa, ya, method='lars', verbose=True)
+coefs = coefs[:, 1:] # coefs starts with an extra column of zeros
+
+# Display the first few variables selected by LARS and the correlation
+# between fitted and observed values
+for k in range(20):
+
+    # k denotes the order in which the variables are added to the model
+    # by LARS.  active[k-1] contains the indicators of the variables in
+    # the model at stage k
+
+    # Print the variables in the order they are selected, and the correlation
+    # coefficients between these fitted values and the observed outcomes.
     f = np.dot(xa, coefs[:, k])
-    print(np.corrcoef(ya, f)[0, 1])
+    print("%-60s %6.2f %8.4f" % (xnames[active[k]],
+                                 np.corrcoef(ya, f)[0, 1],
+                                 coefs[active[k], k]))
 
 # Fixed effects model for provider type
 pfemodelz = sm.OLS.from_formula("log_op_z ~ log_nonop_z + C(provider_type)",
@@ -191,7 +252,7 @@ pferesultz = pfemodelz.fit()
 pmemodelz = sm.MixedLM.from_formula("log_op_z ~ log_nonop_z", groups="provider_type", data=dr)
 pmeresultz = pmemodelz.fit()
 
-# Merge the blups with the fixed effects estimates
+# Merge the BLUPs with the fixed effects estimates
 re = pmeresultz.random_effects
 re = {k: re[k].provider_type for k in re.keys()}
 re = pd.DataFrame({"re": pd.Series(re)})
@@ -204,66 +265,54 @@ for k in pferesultz.params.index:
 fe = pd.Series(fe)
 fe = pd.DataFrame(fe, columns=["fe"])
 refe = pd.merge(fe, re, left_index=True, right_index=True)
+
+# Mean center to eliminate dependence on the reference category
 refe -= refe.mean(0)
+
+# Add in the sample size (per group) so we can see how this relates
+# to the degree of shrinkage
 n = dr.provider_type.value_counts()
 n = pd.DataFrame(n)
 n.columns=["n"]
 refe = pd.merge(refe, n, left_index=True, right_index=True)
 
-# When the number of providers is small, there is more shrinkage in the
-# random effects compared to the fixed effects.
-pt = dr.provider_type.value_counts()
-ii = pt.index[pt < 100]
-refe.loc[ii, :].dropna()
-
-pmemodels = sm.MixedLM.from_formula("log_op_z ~ log_nonop_z",
-                                    groups="provider_type",
+# Mixed model looking at provider types: allow varying intercepts and slopes
+# by provider type
+pmemodels = sm.MixedLM.from_formula("log_op_z ~ log_nonop_z", groups="provider_type",
                                     vc_formula={"i": "0+C(provider_type)",
                                                 "s": "0+log_nonop_z:C(provider_type)"},
                                     data=dr)
 pmeresults = pmemodels.fit()
 
-# Use lars to consider provider effects
-y, x = patsy.dmatrices("log_op_z ~ 0 + log_nonop_z + C(provider_type)", data=dr,
+# Use LARS to consider provider effects and state effects together
+y, x = patsy.dmatrices("log_op_z ~ 0 + log_nonop_z + C(provider_type) + C(state)", data=dr,
                        return_type='dataframe')
+
+# Standardize
 xa = np.asarray(x)
 xa -= xa.mean(0)
 xa /= xa.std(0)
 ya = np.asarray(y)[:,0]
-ya -= ya.mean()
-ya /= ya.std()
+ya -= ya.mean(0)
+ya /= ya.std(0)
 xnames = x.columns.tolist()
+
+# Run LARS for provider and state varying intercepts
 alphas, active, coefs = linear_model.lars_path(xa, ya, method='lars', verbose=True)
+coefs = coefs[:, 1:]
 
-# Display the first few variables selected by lars and the fitted
-# correlation.
-print("\nLARS path for state main effects:")
-for k in range(1, 20):
-    print(xnames[active[k-1]])
-    f = np.dot(xa, coefs[:, k])
-    print(np.corrcoef(ya, f)[0, 1])
-
-pa = pd.Series(coefs[:, 15], index=xnames)
-pa = pa[pa != 0]
-
-# Use lars to consider provider effects and state effects together
-y, x = patsy.dmatrices("log_op_z ~ 0 + log_nonop_z + C(provider_type) + C(state)", data=dr,
-                       return_type='dataframe')
-xa = np.asarray(x)
-ya = np.asarray(y)[:,0]
-xnames = x.columns.tolist()
-alphas, active, coefs = linear_model.lars_path(xa, ya, method='lars', verbose=True)
-
-# Display the first few variables selected by lars and the fitted
-# correlation.
+# Display the first few variables selected by LARS, and the correlations between
+# fitted and observed values for each model selected by LARS.
 print("\nLARS path for provider type and state main effects:")
-for k in range(1, 20):
-    print(xnames[active[k-1]])
+for k in range(20):
     f = np.dot(xa, coefs[:, k])
-    print(k, np.corrcoef(ya, f)[0, 1])
+    print("%-60s %6.2f %8.4f" % (xnames[active[k]],
+                                 np.corrcoef(ya, f)[0, 1],
+                                 coefs[active[k], k]))
 
-pa = pd.Series(coefs[:, 15], index=xnames)
-pa = pa[pa != 0]
+if noproc:
+    import sys
+    sys.exit(0)
 
 #
 # Procedure type analysis
@@ -291,6 +340,8 @@ dr = pd.merge(dr, xr, left_on="npi", right_index=True)
 procs = " + ".join(xr.columns.tolist())
 y, x = patsy.dmatrices("log_op_z ~ 0 + log_nonop_z + %s" % procs,
                        data=dr, return_type='dataframe')
+
+# Standardize the data before running LARS on it
 xa = np.asarray(x)
 ya = np.asarray(y)[:,0]
 xa -= xa.mean(0)
@@ -298,10 +349,12 @@ xa /= xa.std(0)
 ya -= ya.mean()
 ya /= ya.std()
 xnames = x.columns.tolist()
+
+# Run LARS on procedure type factors
 alphas, active, coefs = linear_model.lars_path(xa, ya, method='lars', verbose=True)
 
-# Display the first few variables selected by lars and the fitted
-# correlation.
+# Display the first few variables selected by LARS, and the
+# correlations between fitted and observed values at each step.
 print("\nLARS path for procedure types:")
 for k in range(1, 10):
     print(xnames[active[k-1]])
