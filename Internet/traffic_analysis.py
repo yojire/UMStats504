3ad958358edb2d1e226003d525eed54f6f4f0269
Kerby Shedden <kshedden@umich.edu>
2018-11-13 23:19:38 -0500

    update

--- Internet/traffic_analysis.py
+++ Internet/traffic_analysis.py
@@ -16,7 +16,7 @@ df = pd.read_csv("traffic_stats.csv")
 for i in range(4):
     plt.clf()
     plt.axes([0.17, 0.1, 0.75, 0.8])
-    plt.plot(df.iloc[:, i])
+    plt.plot(df.iloc[:, 2+i])
     plt.grid(True)
     plt.xlabel("Minutes", size=15)
     plt.ylabel(df.columns[i], size=15)
@@ -27,7 +27,7 @@ for i in range(4):
     plt.clf()
     plt.axes([0.17, 0.1, 0.75, 0.8])
     pp = np.linspace(0, 1, df.shape[0])
-    plt.plot(pp, np.sort(df.iloc[:, i]))
+    plt.plot(pp, np.sort(df.iloc[:, 2+i]))
     plt.grid(True)
     plt.xlabel("Probability point", size=15)
     plt.ylabel(df.columns[i] + " quantile", size=15)
@@ -65,33 +65,44 @@ def kt(z, k):
     y = z[k:n]
     return kendalltau(x, y).correlation
 
-# Calculate all the Kenall's tau autocorrelation values (for
+# Calculate all the Kendall's tau autocorrelation values (for
 # each data series, at different lags).
-f = [[], [], [], []]
+f = []
 for j in range(4):
-    for k in range(1, 240):
-        f[j].append(kt(df.iloc[:, j], k))
-    f[j] = np.asarray(f[j])
+    v = []
+    # Difference the series at various orders
+    for diffo in 0, 1, 2:
+        z = df.iloc[:, 2+j]
+        if diffo > 0:
+            z = np.diff(z, diffo)
+        u = []
+        for k in range(1, 241):
+            u.append(kt(z, k))
+        u = np.asarray(u)
+        v.append(u)
+    f.append(v)
+f = np.asarray(f)
 
 # Plot the tau autocorrelations
-plt.clf()
-plt.grid(True)
-plt.plot(f[0], label=df.columns[0])
-plt.plot(f[1], label=df.columns[1])
-plt.plot(f[2], label=df.columns[2])
-plt.plot(f[3], label=df.columns[3])
-plt.xlabel("Minutes", size=15)
-plt.ylabel("Tau-autocorrelation", size=15)
-ha,lb = plt.gca().get_legend_handles_labels()
-leg = plt.figlegend(ha, lb, "upper center", ncol=4)
-leg.draw_frame(False)
-pdf.savefig()
+for k in range(3):
+    plt.clf()
+    plt.grid(True)
+    for j in range(4):
+        x = np.arange(k, 240)
+        plt.plot(x, f[j, k, k:], label=df.columns[j])
+    plt.xlabel("Minutes", size=15)
+    plt.ylabel("Tau-autocorrelation", size=15)
+    plt.title("Lag %d" % k)
+    ha,lb = plt.gca().get_legend_handles_labels()
+    leg = plt.figlegend(ha, lb, "upper center", ncol=4)
+    leg.draw_frame(False)
+    pdf.savefig()
 
 
 # Calculate the Hurst index using means and variances.
 def hurst(x):
     z = []
-    for m in 15,30,60:
+    for m in 15, 30, 60:
         y = np.reshape(x, (-1, m))
         v = y.mean(1).var()
         z.append([m, v])
@@ -103,7 +114,7 @@ def hurst(x):
 # Calculate the Hurst index using absolute values.
 def hurstabs(x):
     z = []
-    for m in 15,30,60:
+    for m in 15, 30, 60:
         y = np.reshape(x, (-1, m))
         v = np.mean(np.abs(y.mean(1)))
         z.append([m, v])
@@ -113,10 +124,15 @@ def hurstabs(x):
     return b + 1
 
 # Calculate the Hurst index two ways, for all four data series.
-for x in df.Traffic, df.Sources, df.UDP, df.TCP:
-    x = np.log(np.asarray(x))
-    x -= x.mean()
-    print("%.3f %.3f" % (hurst(x), hurstabs(x)))
+print("Hurst parameters:")
+for j, x in enumerate([df.Traffic, df.Sources, df.UDP, df.TCP]):
+    print(df.columns[2+j])
+    for diffo in range(3):
+        x = np.asarray(x, dtype=np.float64)
+        z = np.diff(x, diffo)
+        z = z[0:60*(len(z)//60)]
+        z -= z.mean()
+        print("    %4d %7.3f %7.3f" % (diffo, hurst(z), hurstabs(z)))
 
 from numpy.lib.stride_tricks import as_strided
 import statsmodels.api as sm
@@ -129,19 +145,39 @@ for j,x in enumerate([df.Traffic, df.Sources, df.UDP, df.TCP]):
     z = as_strided(x, shape=(len(x)-30, 30), strides=(8, 8))
     y = z[:, 0]
     x = z[:, 1:30]
-    params = []
-    for alpha in 0.0001, 0.0002:
-        result = sm.OLS(y, x).fit_regularized(alpha=0.1, L1_wt=0.1)
-        params.append(result.params)
 
-    plt.clf()
-    plt.title(labs[j])
-    for p in params:
-        plt.plot(p)
-    plt.xlabel("Lag", size=15)
-    plt.ylabel("Coefficient", size=15)
-    plt.grid(True)
-    plt.ylim(-1, 1)
-    pdf.savefig()
+    for jj in 0, 1, 2, 3:
+
+        params = []
+
+        if jj == 0:
+            title = "OLS"
+            result = sm.OLS(y, x).fit()
+            params.append(result.params)
+        elif jj == 1:
+            title = "Ridge"
+            for alpha in 0.001, 0.01, 0.1:
+                result = sm.OLS(y, x).fit_regularized(alpha=alpha, L1_wt=0)
+                params.append(result.params)
+        elif jj == 2:
+            title = "Lasso"
+            for alpha in 0.001, 0.01, 0.1:
+                result = sm.OLS(y, x).fit_regularized(alpha=alpha, L1_wt=1)
+                params.append(result.params)
+        elif jj == 3:
+            title = "Elastic net"
+            for alpha in 0.00001, 0.001, 0.1:
+                result = sm.OLS(y, x).fit_regularized(alpha=alpha, L1_wt=0.1)
+                params.append(result.params)
+
+        plt.clf()
+        plt.title(labs[j] + " " + title)
+        for p in params:
+            plt.plot(p)
+        plt.xlabel("Lag", size=15)
+        plt.ylabel("Coefficient", size=15)
+        plt.grid(True)
+        plt.ylim(-1, 1)
+        pdf.savefig()
 
 pdf.close()
