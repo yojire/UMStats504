d4318024292d5eacfe491dcb1adc63e8805da5de
Kerby Shedden <kshedden@umich.edu>
2018-12-01 15:08:53 -0500

    update

--- NHANES-actigraphy/actigraphy_idv_prep.py
+++ NHANES-actigraphy/actigraphy_idv_prep.py
@@ -1,5 +1,5 @@
 """
-Compute the eigenvalues from a Singular spectrum analysis (SSA) of the NHANES
+Compute the intra-daily variation (IDV) values for the NHANES
 actigraphy data, and save them to a file.
 """
 
